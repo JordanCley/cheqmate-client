b9773f81601ff63d1b4c78332d26e8ac53a1e398
Jordan McQuiston <jordan@jordanmcquiston.com>
2020-05-02 19:15:30 -0700

    Hard coded in a version number

--- src/components/FooterComponent.js
+++ src/components/FooterComponent.js
@@ -8,7 +8,7 @@ const FooterComponent = () => {
   return (
     <Container id={"footer"}>
       <p>
-        &copy; 2020 Cheqmate Inc. All Rights Reserved. Version 1
+        &copy; 2020 Cheqmate Inc. All Rights Reserved. version 1.0.1
         <img src={logo} className={"q-logo ml-1 mr-2"} alt={"Q-logo"} />
       </p>
     </Container>
