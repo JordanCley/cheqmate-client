23f0f39623590af56b7d11b651cf7394ada8a035
Jordan McQuiston <jordan@jordanmcquiston.com>
2020-05-06 20:56:03 -0700

    Fixed nav margin for desktop

--- src/components/FooterComponent.js
+++ src/components/FooterComponent.js
@@ -8,7 +8,7 @@ const FooterComponent = () => {
   return (
     <Container id={"footer"}>
       <p>
-        &copy; 2020 Cheqmate Inc. All Rights Reserved. version 1.0.2
+        &copy; 2020 Cheqmate Inc. All Rights Reserved. version 1.0.3
         <img src={logo} className={"q-logo ml-1 mr-2"} alt={"Q-logo"} />
       </p>
     </Container>
